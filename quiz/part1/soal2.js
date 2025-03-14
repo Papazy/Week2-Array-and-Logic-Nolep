8c577846d9cb63aad7ab618dd4804e09f58804bd
Papazy <fajryjobs@gmail.com>
2025-03-15 03:14:39 +0700

    fix soal 2

--- quiz/part1/soal2.js
+++ quiz/part1/soal2.js
@@ -7,10 +7,49 @@ function dataHandling(input) {
   console.log(input)
 
   date = input[3].split("/");
-  let reverseDate = [];
-  for(let i = date.length - 1; i >= 0; i--){
-    reverseDate.push(date[i])
+
+  switch(date[1]) {
+    case "01":
+      console.log("Januari");
+      break;
+    case "02":
+      console.log("Februari");
+      break;
+    case "03":
+      console.log("Maret");
+      break;
+    case "04":
+      console.log("April");
+      break;
+    case "05":
+      console.log("Mei");
+      break;
+    case "06":
+      console.log("Juni");
+      break;
+    case "07":
+      console.log("Juli");
+      break;
+    case "08":
+      console.log("Agustus");
+      break;
+    case "09":
+      console.log("September");
+      break;
+    case "10":
+      console.log("Oktober");
+      break;
+    case "11":
+      console.log("November");
+      break;
+    case "12":
+      console.log("Desember");
+      break;
+    default:
+      console.log("Bulan tidak valid");
   }
+
+  let reverseDate = [date[2], date[0], date[1]]
   console.log(reverseDate)
 
   console.log(date.join("-"))
